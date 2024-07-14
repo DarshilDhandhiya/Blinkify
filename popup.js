62b2baaece682c1182b824b7abe14ace5fee920c
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2024-07-14 18:54:21 +0530

    Code Added

---
+++ popup.js
@@ -0,0 +1,24 @@
+document.getElementById('shorten-button').addEventListener('click', () => {
+    const url = document.getElementById('url-input').value;
+  
+    if (url) {
+      fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(url)}`)
+        .then(response => response.text())
+        .then(shortUrl => {
+          const resultElement = document.getElementById('result');
+          resultElement.textContent = shortUrl;
+  
+          navigator.clipboard.writeText(shortUrl).then(() => {
+            console.log('Short URL copied to clipboard');
+          }).catch(err => {
+            console.error('Could not copy text: ', err);
+          });
+        })
+        .catch(error => {
+          console.error('Error shortening URL: ', error);
+        });
+    } else {
+      alert('Please paste a URL first.');
+    }
+  });
+  
